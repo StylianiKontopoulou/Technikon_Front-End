82a391e2ad60ef62a850b067657c32bf40012bbc
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-09-28 16:31:34 +0300

    nav-bar implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { AdminComponent } from "./admin/admin.component";
 import { UserComponent } from "./user/user.component";
 import { LoginFormComponent } from "./login-form/login-form.component";
 import { SelfRegistrationPageComponent } from "./self-registration-page/self-registration-page.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminComponent, UserComponent, LoginFormComponent, SelfRegistrationPageComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, AdminComponent, UserComponent, LoginFormComponent, SelfRegistrationPageComponent, RouterLink, RouterLinkActive, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
