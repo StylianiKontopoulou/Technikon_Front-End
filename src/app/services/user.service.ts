21835298610a5f3f2bb0b23c9bdc8bd675506916
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-10-03 00:41:57 +0300

    CRUD implementation for the Property Owner user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { retry, catchError, throwError } from 'rxjs';
+import { retry, catchError, throwError, Observable, pipe } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,74 @@ import { retry, catchError, throwError } from 'rxjs';
 export class UserService {
 
   http = inject(HttpClient);
+  private baseUrl = 'http://localhost:8080/webTechnikon/resources';
 
-  getUser() {
-    const url ='http://localhost:8080/webTechnikon/resources/Users/property-owners';
-    return this.http.get(url);
+    // Δημιουργία headers για Basic Auth
+    private createAuthorizationHeader(): HttpHeaders {
+      const auth = 'Basic ' + localStorage.getItem('authorizationHeader');
+      return new HttpHeaders({ Authorization: auth });
+    }
+
+  getUser(userId: any): Observable<any> {
+    const headers = this.createAuthorizationHeader();
+    return this.http.get(`${this.baseUrl}/users/${userId}`, {headers});
   }
 
   login(data: any){
-    const url ='http://localhost:8080/webTechnikon/resources/users/login';
+    const url =`${this.baseUrl}/users/login`;
     const headers = new HttpHeaders()
     .set('Content-Type', 'application/json')
 
-    return this.http.post(url,JSON.stringify(data), {headers:headers})
+    return this.http.post(url,JSON.stringify(data), {headers})
     .pipe(
       retry(1),
       catchError(error => throwError(() => 'Something is wrong...')) 
     );
   }
+
+  // Κλήση για λήψη όλων των properties με Basic Auth
+  getAllMyProperties(userId: any): Observable<any> {
+    const headers = this.createAuthorizationHeader();
+    const url = `${this.baseUrl}/properties?userId=${userId}`;
+    return this.http.get(url, { headers });
+   
+  }
+
+  // Κλήση για προσθήκη νέου property με Basic Auth
+  postAddProperty(property: any): Observable<any> {
+    const headers = this.createAuthorizationHeader().set(
+      'Content-Type',
+      'application/json'
+    );
+    const url = `${this.baseUrl}/properties`;
+    return this.http.post(url, JSON.stringify(property), { headers }).pipe(
+      retry(1),
+      catchError((error) => throwError(() => 'Something is wrong...'))
+    );
+  }
+
+  getMyRepairs(propertyId: number): Observable<any> {
+    const headers = this.createAuthorizationHeader();
+    return this.http.get(`${this.baseUrl}/repairs?propertyId=${propertyId}`, {headers});
+  }
+
+  softDeleteUser(userId: any): Observable<any> {
+    const headers = this.createAuthorizationHeader();
+    return this.http.put(`${this.baseUrl}/users/${userId}/deactivate`, {headers});
+  }
+  
+
+  putUpdateUser(userId: any, userData: any): Observable<any>{
+    const url = `${this.baseUrl}/users/${userId}`;
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+    return this.http.put(url, JSON.stringify(userData), { headers })
+      .pipe(
+        retry(1),
+        catchError(error => throwError(() => 'Failed to update user...'))
+      );
+  }
+  
+
 }
 
