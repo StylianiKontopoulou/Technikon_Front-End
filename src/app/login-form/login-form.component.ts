62de63522caf2ecb9dc1c43685b1e0b42af1b9cd
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-10-03 00:29:48 +0300

    fixed route parameter

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -9,70 +9,7 @@ import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
 
-// @Component({
-//   selector: 'app-login-form',
-//   standalone: true,
-//   imports: [ReactiveFormsModule],
-//   templateUrl: './login-form.component.html',
-//   styleUrl: './login-form.component.css',
-// })
-// export class LoginFormComponent implements OnInit {
-//   loginForm!: FormGroup;
-//   fb = inject(FormBuilder);
-//   userService = inject(UserService);
 
-//   ngOnInit(): void {
-//     this.loginForm = this.fb.group({
-//       userName: ['', [Validators.required]],
-//       password: [
-//         '',
-//         [
-//           Validators.required,
-//           // Validators.minLength(4),
-//           // Validators.pattern('^[a-zA-Z]+$'),
-//         ],
-//       ],
-//     }); //ειναι αρχικη τιμη στη φορμα ο,τι γραψω μεσα
-//   }
-
-//   get userName() {
-//       return this.loginForm.get('userName');
-
-//   }
-
-//   get password() {
-//     return this.loginForm.get('password');
-//   }
-
-//   loginUser() {
-
-//     this.userService.login(this.loginForm.value).subscribe({
-//       next: (response: any) => {
-//         localStorage.setItem(
-//           'authorizationHeader',
-//           btoa(`${this.userName?.value}:${this.password?.value}`)
-//         );
-//         localStorage.setItem('userId', response.id);
-//         localStorage.setItem('userType', response.userType);
-//         const userRole = response.userType; //ADMIN,PROPERTY_OWNER
-//         this.navigateTo(userRole);
-//       },
-//       error: (err) => console.error(`Something is wrong... ${err}`),
-//     });
-//   }
-//   router = inject(Router);
-//   goSignUp() {
-//     this.router.navigate(['home']);
-//   }
-
-//   navigateTo(userRole: String) {
-//     if (userRole === 'ADMIN') {
-//       this.router.navigate(['admin']);
-//     } else if (userRole === 'PROPERTY_OWNER'){
-//       this.router.navigate(['user/:id']);
-//     }
-//   }
-// }
 @Component({
   selector: 'app-login-form',
   standalone: true,
@@ -116,7 +53,11 @@ export class LoginFormComponent implements OnInit {
         this.authService.login();
 
         const userRole = response.userType;
-        this.navigateTo(userRole);
+        if (userRole === 'ADMIN') {
+          this.router.navigate(['admin']);
+        } else if (userRole === 'PROPERTY_OWNER') {
+          this.router.navigate([`user/${response.id}`]);
+        }
       },
       error: (err) => console.error(`Something is wrong... ${err}`),
     });
@@ -127,11 +68,5 @@ export class LoginFormComponent implements OnInit {
     this.router.navigate(['signup']);
   }
 
-  navigateTo(userRole: string) {
-    if (userRole === 'ADMIN') {
-      this.router.navigate(['admin']);
-    } else if (userRole === 'PROPERTY_OWNER') {
-      this.router.navigate(['user/:id']);
-    }
-  }
+
 }
