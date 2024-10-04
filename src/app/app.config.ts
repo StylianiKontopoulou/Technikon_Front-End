3b03a94a8fc7405e3c8e1111bcead94521b0baaa
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-10-04 14:57:00 +0300

    final

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { RouterLink, provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  RouterLink,
+  provideRouter,
+  withComponentInputBinding,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient(), RouterLink]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    RouterLink,
+  ],
 };
